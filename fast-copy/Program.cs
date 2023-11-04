dbd3f7aec8e0686e9e732c6b7fd1c7e3080b7321
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-11-03 21:32:03 -0700

    wip

--- fast-copy/Program.cs
+++ fast-copy/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using Microsoft.Extensions.ObjectPool;
+
+string source = args?[0] ?? throw new ArgumentNullException(nameof(source));
+string dest = args?[1] ?? throw new ArgumentNullException(nameof(dest));
+
+int threads = 12;
+
+Console.WriteLine($"Copying {source} to {dest}");
+
+ObjectPool<Chunk> objectPool = ObjectPool.Create<Chunk>();
+FileInfo fileInfo = new FileInfo(source);
+
+static async Task ReadAsync(
+    string source,
+    ChannelWriter<Chunk> channelWriter,
+    ObjectPool<Chunk> objectPool)
+{
+    using (FileStream fileStream = File.OpenRead(source))
+    {
+        while (true)
+        {
+            Chunk chunk = objectPool.Get();
+            chunk.
+        }
+    }
+}
+
+public class Chunk
+{
+    public byte[] Buffer { get; } = new byte[516 * 1024];
+    
+
+}
