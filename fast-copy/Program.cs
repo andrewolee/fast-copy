937b8139252bb24b03aa519ef9c4d31393e0ec4d
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-11-04 20:13:26 -0700

    wip

--- fast-copy/Program.cs
+++ fast-copy/Program.cs
@@ -1,14 +1,20 @@
-﻿using System.Threading.Channels;
+﻿using System.IO.MemoryMappedFiles;
+using System.Threading.Channels;
+
 using Microsoft.Extensions.ObjectPool;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: ");
+}
 
 string source = args?[0] ?? throw new ArgumentNullException(nameof(source));
 string dest = args?[1] ?? throw new ArgumentNullException(nameof(dest));
 
 int threads = 12;
 
-Console.WriteLine($"Copying {source} to {dest}");
-
 ObjectPool<Chunk> objectPool = ObjectPool.Create<Chunk>();
+Channel<Chunk> channel = Channel.CreateBounded<Chunk>(threads * 4);
 FileInfo fileInfo = new FileInfo(source);
 
 static async Task ReadAsync(
@@ -21,11 +27,32 @@ static async Task ReadAsync(
         while (true)
         {
             Chunk chunk = objectPool.Get();
+            chunk.Offset = fileStream.Position;
+            chunk.Count = await fileStream.ReadAsync(chunk.Buffer);
+            await channelWriter.WriteAsync(chunk);
         }
     }
 }
 
-public class Chunk
+static async Task WriteAsync(
+    MemoryMappedFile dest,
+    ChannelReader<Chunk> channelReader,
+    ObjectPool<Chunk> objectPool)
 {
-    public byte[] Buffer { get; } = new byte[516 * 1024];
+    while (await channelReader.WaitToReadAsync())
+    {
+        if (channelReader.TryRead(out Chunk chunk))
+        {
+
+        }
+    }
+}
+
+internal class Chunk
+{
+    public byte[] Buffer { get; } = new byte[];
+
+    public int Count { get; set; }
+
+    public long Offset { get; set; }
 }
